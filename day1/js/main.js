d30c7122261be1fd3e17017bb1442083100e5944
Alexander <bureck400@gmail.com>
2023-03-09 17:14:13 +0000

    all done

--- day1/js/main.js
+++ day1/js/main.js
@@ -6,16 +6,27 @@
     console.log(s)
      const audio = document.querySelector(`audio[data-key='${s.keyCode}']`)
      const key = document.querySelector(`div[data-key='${s.keyCode}']`)
-     console.log(audio)
+    //  console.log(audio)
      if (!audio) return;  //stop the function from running all together
      audio.currentTime = 0 
     audio.play()
-    console.log(key)
+    // console.log(key)
 
        key.classList.add('plays')
-
+      
    }
+ // adding the transition end event
 
+ const allKeys = document.querySelectorAll('.key')
 
+        allKeys.forEach(k => k.addEventListener('transitionend', moveTransition))
 
+        function moveTransition(ms) {
+       if (ms.propertyName !==  'transform') {
+          return
+       }
+     console.log(ms.propertyName)
+            this.classList.remove('plays')
+            
+        }
    
